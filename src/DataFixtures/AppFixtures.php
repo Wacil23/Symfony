269ca35297b5158117b3517f601293dbf762a6e8
Wacil23 <wacilzek00@gmail.com>
2022-04-11 05:28:23 +0200

    Challenge7 finished

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'Aventure',
+        'Action',
+        'Horreur',
+        'Comédie',
+        'Science-Fiction',
+        'Drame',
+    ];
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CATEGORIES as $key => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+}
